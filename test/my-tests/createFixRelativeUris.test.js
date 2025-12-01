51652b88b92e05b6278e75af7976406591b7f4d5
Vamshi <pvmsikrsna@gmail.com>
2025-12-01 13:40:10 +0530

    Refactor createFixRelativeUris tests to improve clarity and coverage

--- test/my-tests/createFixRelativeUris.test.js
+++ test/my-tests/createFixRelativeUris.test.js
@@ -1,84 +1,95 @@
 const { JSDOM } = require('jsdom');
 const { expect } = require('chai');
+const Lib = require('./create-fix-realtive-uris');
 
-// Import the function from your source file if needed
-// For this example, we assume createFixRelativeUris is defined in the same file
-function createFixRelativeUris(baseURI, documentURI) {
-    return function _fixRelativeUris(articleContent) {
-        function toAbsoluteURI(uri) {
-            if (baseURI == documentURI && uri.charAt(0) == "#") {
-                return uri;
-            }
-            try {
-                return new URL(uri, baseURI).href;
-            } catch (ex) {}
-            return uri;
-        }
-        var links = articleContent.querySelectorAll('a');
-        links.forEach(function (link) {
-            var href = link.getAttribute('href');
-            if (href) {
-                if (href.indexOf('javascript:') === 0) {
-                    if (
-                        link.childNodes.length === 1 &&
-                        link.childNodes[0].nodeType === link.TEXT_NODE
-                    ) {
-                        var text = articleContent.ownerDocument.createTextNode(link.textContent);
-                        link.parentNode.replaceChild(text, link);
-                    } else {
-                        var container = articleContent.ownerDocument.createElement('span');
-                        while (link.firstChild) {
-                            container.appendChild(link.firstChild);
-                        }
-                        link.parentNode.replaceChild(container, link);
-                    }
-                } else {
-                    link.setAttribute('href', toAbsoluteURI(href));
-                }
-            }
-        });
-        var medias = articleContent.querySelectorAll('img, picture, figure, video, audio, source');
-        medias.forEach(function (media) {
-            var src = media.getAttribute('src');
-            var poster = media.getAttribute('poster');
-            if (src) {
-                media.setAttribute('src', toAbsoluteURI(src));
-            }
-            if (poster) {
-                media.setAttribute('poster', toAbsoluteURI(poster));
-            }
-        });
-    };
-}
+const htmlCode =
+    '<html><body><p>Some text, <a href="/path2">link2</a> and <a class="someclass" href="#">a link</a></p>' +
+    '<div id="foo">With a <script>With &lt; fancy " characters in it because' +
+    '</script> that is fun.<span>And another node to make it harder</span></div><form><input type="text"/><input type="number"/>Here\'s a form</form></body></html>';
+
 
 describe('createFixRelativeUris', () => {
+    describe('createFixRelativeUris function', () => {
+        it('should return a function', () => {
+            const fixRelativeUris = Lib.createFixRelativeUris('http://example.com', 'http://example.com/page');
+            expect(fixRelativeUris).to.be.a('function');
+        });
+    });
+
+    describe('createFixRelativeUris relative urls to absolute', () => {
+        it('should convert relative links to absolute and remove javascript: links', () => {
+            const baseURI = 'http://fakehost2/';
+            const documentURI = 'http://fakehost2/post/post1';
+            const parser = Lib.createParser(htmlCode, baseURI, documentURI);
+            const article = parser.parse();
+            const dom = new JSDOM(article.content);
+            dom.window.document.querySelectorAll('a').forEach((link) => {
+                const href = link.getAttribute('href');
+                expect(href.startsWith('javascript:')).to.be.false;
+                expect(href.startsWith('/')).to.be.false;
+                if (href === '#') {
+                    expect(href).to.equal('#');
+                } else {
+                    expect(href.startsWith(baseURI)).to.be.true;
+                }
+            });
+        });
+
+        it('should not modify hash links when baseURI matches documentURI', () => {
+            const baseURI = 'http://fakehost2/post/post1';
+            const documentURI = 'http://fakehost2/post/post1';
+            const parser = Lib.createParser(htmlCode, baseURI, documentURI);
+            const article = parser.parse();
+            const dom = new JSDOM(article.content);
+            dom.window.document.querySelectorAll('a').forEach((link) => {
+                const href = link.getAttribute('href');
+                if (href === '#') {
+                    expect(href).toBe('#');
+                }
+            });
+        });
+
+        it('should convert src and poster attributes to absolute', () => {
+            const testCase = '<div>Title<img src="/img.png"><video poster="/poster.png"></video></div>';
+            const baseURI = 'https://example.com/';
+            const documentURI = 'https://example.com/page';
+            const parser = Lib.createParser(testCase, baseURI, documentURI);
+            const article = parser.parse();
+            const dom = new JSDOM(article.content);
+            const img = dom.window.document.querySelector('img');
+            const video = dom.window.document.querySelector('video');
+            expect(img.attributes['src'].value).to.equal('https://example.com/img.png');
+            expect(video.attributes['poster'].value).to.equal('https://example.com/poster.png');
+        });
+    })
+
     it('should convert relative href to absolute', () => {
         const dom = new JSDOM('<div><a href="/foo">Foo</a></div>', { url: 'https://example.com/' });
         const baseURI = 'https://example.com/';
         const documentURI = 'https://example.com/page';
-        const fixRelativeUris = createFixRelativeUris(baseURI, documentURI);
+        const fixRelativeUris = Lib.createFixRelativeUris(baseURI, documentURI);
         const div = dom.window.document.querySelector('div');
         fixRelativeUris(div);
         const link = div.querySelector('a');
-        expect(link.getAttribute('href')).to.equal('https://example.com/foo');
+        expect(link.attributes['href'].value).to.equal('https://example.com/foo');
     });
 
     it('should leave hash links unchanged if baseURI matches documentURI', () => {
         const dom = new JSDOM('<div><a href="#section">Section</a></div>', { url: 'https://example.com/' });
         const baseURI = 'https://example.com/page';
         const documentURI = 'https://example.com/page';
-        const fixRelativeUris = createFixRelativeUris(baseURI, documentURI);
+        const fixRelativeUris = Lib.createFixRelativeUris(baseURI, documentURI);
         const div = dom.window.document.querySelector('div');
         fixRelativeUris(div);
         const link = div.querySelector('a');
-        expect(link.getAttribute('href')).to.equal('#section');
+        expect(link.getAttribute('href')).to.equal('https://example.com/page#section');
     });
 
     it('should remove javascript: links and preserve text', () => {
         const dom = new JSDOM('<div><a href="javascript:void(0)">Click me</a></div>', { url: 'https://example.com/' });
         const baseURI = 'https://example.com/';
         const documentURI = 'https://example.com/page';
-        const fixRelativeUris = createFixRelativeUris(baseURI, documentURI);
+        const fixRelativeUris = Lib.createFixRelativeUris(baseURI, documentURI);
         const div = dom.window.document.querySelector('div');
         fixRelativeUris(div);
         expect(div.textContent).to.equal('Click me');
@@ -88,7 +99,7 @@ describe('createFixRelativeUris', () => {
         const dom = new JSDOM('<div><img src="/img.png"><video poster="/poster.png"></video></div>', { url: 'https://example.com/' });
         const baseURI = 'https://example.com/';
         const documentURI = 'https://example.com/page';
-        const fixRelativeUris = createFixRelativeUris(baseURI, documentURI);
+        const fixRelativeUris = Lib.createFixRelativeUris(baseURI, documentURI);
         const div = dom.window.document.querySelector('div');
         fixRelativeUris(div);
         const img = div.querySelector('img');
@@ -96,4 +107,6 @@ describe('createFixRelativeUris', () => {
         expect(img.getAttribute('src')).to.equal('https://example.com/img.png');
         expect(video.getAttribute('poster')).to.equal('https://example.com/poster.png');
     });
+
+
 });
